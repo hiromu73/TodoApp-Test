5accc3c5a6ec7515550668ee697ef9a92a893dc2
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-09-21 18:45:07 +0900

    Update todos_state.dart

--- lib/todos_state.dart
+++ lib/todos_state.dart
@@ -14,15 +14,16 @@ part 'todos_state.freezed.dart';
 
 
 @freezed
-abstract class TodosState with _$TodosState {
-  const factory TodosState({//TodosStateのクラスをチェック
+abstract class TodosState with _$TodosState  {
+  const factory TodosState( {//TodosStateのクラスをチェック
     @Default(<Todo>[]) List<Todo> todos,//すべてのTodoが入るtodos
   }) = TodosStateData;//TodosStateDataであればデータの読み込みが終わったという判断ができるように
   const factory TodosState.loading() = TodosStateLoading;//TodosStateLoadingであればまだ読み込み中
 }
 
 //ここからDB
-class TodosController extends StateNotifier<TodosState> with LocatorMixin {//LocatorMixinをmixinする事でcontextにあるproviderへのアクセスを容易する
+class TodosController extends StateNotifier<TodosState> with LocatorMixin  {
+//LocatorMixinをmixinする事でcontextにあるproviderへのアクセスを容易する
   TodosController() : super(const TodosState.loading());
 
   final _uuid = Uuid();
@@ -30,32 +31,41 @@ class TodosController extends StateNotifier<TodosState> with LocatorMixin {//Loc
   @override
   void initState() async {
     super.initState();
-
     await Future<void>.delayed(const Duration(seconds: 3));//initStateで3秒間ウエイトを入れ,初期データとしていくつかのTodoをstateへ設定
-
     // 初期データを設定、TodosStateLoadingからTodoStateDataへ変わるのでローディング完了の状態となる
+
+    var prefs = await SharedPreferences.getInstance();
     state = TodosState(
-      todos: [
-        Todo(id: _uuid.v4(), title: 'テスト'),
-        Todo(id: _uuid.v4(), title: 'TodoApp'),
-        Todo(id: _uuid.v4(), title: 'provider'),
-      ],
-    );
+    todos: [
+      Todo(id: _uuid.v4(), title: 'テスト'),
+      Todo(id: prefs.getString('_uuid.v4()') , title: prefs.getString('title') ),
+      Todo(id: prefs.getString('_uuid.v4()') , title: prefs.getString('title') ),
+      Todo(id: prefs.getString('_uuid.v4()') , title: prefs.getString('title') ),
+    ],
+      );
   }
 
-  void add(String title) {//追加機能
-    final currentState = state;//
+  void add(String title) async {//追加機能
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final currentState =  state;
     if (currentState is TodosStateData) {
       // todosのクローンに新しいTodoを追加してstateを更新
       final todos = currentState.todos.toList()
-        ..add(
+    ..add(
           Todo(id: _uuid.v4(), title: title),
-        );
+    );
+      prefs.setString('id',_uuid.v4());
+      prefs.setString('title',title) ;
+      prefs.setString('id',_uuid.v4());
+      prefs.setString('title',title) ;
       state = currentState.copyWith(//stateはimmutableでメンバ変数を直接変更することはできないので、stateを更新するときは現在のstateからcopyWithでコピーするか、新規のstateで上書きする
         todos: todos,
       );
+
     }
   }
+
+
 
   void toggle(Todo todo) {
     final currentState = state;
